85488ecfc48382498c70ce2b204a47e406d6cdd8
jason1chiu <119711904+jason1chiu@users.noreply.github.com>
2023-02-19 02:02:20 -0500

    created license section and tested it

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # README Generator
   
+  ## License
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT) The application is covered under the [MIT License](https://opensource.org/licenses/MIT)
+
   ## Description
   My motivation to create a README Generator is to encourage developers to always include professioanl README.md files for their projects so that users understand what their projects do. I built this project because I want to let developers like myself to easily generate README.md files describing projects such as this one. It solves the problem to create a detailed README.md file that should include the title, description, installation instructions, usage, credits and license for a project by asking a series of questions that developers should ask themselves when their project is ready to share. I learned how to use the command line in the termin to ask a few questions to generate a descriptive README.md file using JavaScript, node.js, and npm.
 
@@ -8,10 +11,9 @@
   1. [Installation](#installation)
   2. [Usage](#usage)
   3. [Credits](#credits)
-  4. [License](#license)
 
   ## Installation
-  Go to this GitHub repository
+  1. Go to this [GitHub repository](https://github.com/jason1chiu/proREADMEgenerator/tree/main)<br>2. Git clone this repo using HTTPS or SSH.
 
   ## Usage
   This README.md file is an example itself where I provided instructions on how to install and use my README Generator.
@@ -25,3 +27,4 @@
 
   ### Tutorials
   None
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -35,31 +35,31 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === 'Apache License 2.0') {
-    return `The application is covered under the [Apache License 2.0](https://opensource.org/licenses/Apache-2.0)`;
+    return `The application is covered under the [Apache License 2.0](https://opensource.org/licenses/Apache-2.0).`;
   } else if (license === 'GNU General Public License v3.0') {
-    return `The application is covered under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0)`;
+    return `The application is covered under the [GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0).`;
   } else if (license === 'MIT License') {
     return `The application is covered under the [MIT License](https://opensource.org/licenses/MIT)`;
   } else if (license === 'BSD 2-Clause "Simplified" License') {
-    return `The application is covered under the [BSD 2-Clause "Simplified" License](https://opensource.org/licenses/BSD-2-Clause)`;
+    return `The application is covered under the [BSD 2-Clause "Simplified" License](https://opensource.org/licenses/BSD-2-Clause).`;
   } else if (license === 'BSD 3-Clause "New" or "Revised" License') {
-    return `The application is covered under the [BSD 3-Clause "New" or "Revised" License](https://opensource.org/licenses/BSD-2-Clause)`;
+    return `The application is covered under the [BSD 3-Clause "New" or "Revised" License](https://opensource.org/licenses/BSD-2-Clause).`;
   } else if (license === 'Boost Software License 1.0') {
-    return `The application is covered under the [Boost Software License 1.0](https://www.boost.org/LICENSE_1_0.txt)`;
+    return `The application is covered under the [Boost Software License 1.0](https://www.boost.org/LICENSE_1_0.txt).`;
   } else if (license === 'Creative Commons Zero v1.0 Universal') {
-    return `The application is covered under the [Creative Commons Zero v1.0 Universal](http://creativecommons.org/publicdomain/zero/1.0/)`;
+    return `The application is covered under the [Creative Commons Zero v1.0 Universal](http://creativecommons.org/publicdomain/zero/1.0/).`;
   } else if (license === 'Eclipse Public License 2.0') {
-    return `The application is covered under the [Eclipse Public License 2.0](https://opensource.org/licenses/EPL-1.0)`;
+    return `The application is covered under the [Eclipse Public License 2.0](https://opensource.org/licenses/EPL-1.0).`;
   } else if (license === 'GNU Affero General Public License v3.0') {
-    return `The application is covered under the [GNU Affero General Public License v3.0](https://www.gnu.org/licenses/agpl-3.0)`;
+    return `The application is covered under the [GNU Affero General Public License v3.0](https://www.gnu.org/licenses/agpl-3.0).`;
   } else if (license === 'GNU General Public License v2.0') {
-    return `The application is covered under the [GNU General Public License v2.0](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+    return `The application is covered under the [GNU General Public License v2.0](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html).`;
   } else if (license === 'GNU Lesser General Public License v3.0') {
-    return `The application is covered under the [GNU Lesser General Public License v3.0](https://www.gnu.org/licenses/lgpl-3.0)`;
+    return `The application is covered under the [GNU Lesser General Public License v3.0](https://www.gnu.org/licenses/lgpl-3.0).`;
   } else if (license === 'Mozilla Public License 2.0') {
-    return `The application is covered under the [Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0)`;
+    return `The application is covered under the [Mozilla Public License 2.0](https://opensource.org/licenses/MPL-2.0).`;
   } else if (license === 'The Unilicense') {
-    return `The application is covered under the [The Unilicense](http://unlicense.org/)`;
+    return `The application is covered under the [The Unilicense](http://unlicense.org/).`;
   } else
     return `[![License: NA]]`;
 }
@@ -67,13 +67,25 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-
+  if (license !== 'None') {
+    let badge = renderLicenseBadge(license);
+    let link = renderLicenseLink(license);
+    return `${badge} ${link}`
+  } else {
+    return `None`
+  }
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+
+  let licenseSect = renderLicenseSection(data.license);
+
   return `# ${data.title}
   
+  ## License
+  ${licenseSect}
+
   ## Description
   ${data.motivation} ${data.reason} ${data.solution} ${data.learn}
 
@@ -82,7 +94,8 @@ function generateMarkdown(data) {
   1. [Installation](#installation)
   2. [Usage](#usage)
   3. [Credits](#credits)
-  4. [License](#license)
+  4. [Test](#test)
+  5. [Questions](#questions)
 
   ## Installation
   ${data.installation}
@@ -90,7 +103,7 @@ function generateMarkdown(data) {
   ## Usage
   ${data.usage}
 
-  ## Credits
+  ## Contributing
   ### Collaborators
   ${data.collaborators}
 
@@ -99,7 +112,16 @@ function generateMarkdown(data) {
 
   ### Tutorials
   ${data.tutorials}
-`;
+
+  ## Features
+
+  ## How to Contribute
+
+  ## Test
+
+  ## Questions
+  `
+  
 }
 
 module.exports = generateMarkdown;
